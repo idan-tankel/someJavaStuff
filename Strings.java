61e4d37f7ac4e9c4f10227c2a44ffd0c8c867d85
idan-tankel <shakel002@gmail.com>
2021-01-23 19:53:55 +0200

    comparing by trimming the string from the start

--- Strings.java
+++ Strings.java
@@ -1,16 +1,27 @@
 public class Strings {
     public static boolean daughter(String s1, String s2){
+        // if some of the strings are null for some reason
+        if(s2.length() == 0 || s2 == null)
+            return true;
+
+        if(s1 == null) // and s2 is not null because of the statement above
+            return false;
+
         // first, check the length of the two strings
         if(s1.length()<s2.length())
             return false;
-        if(s1.length() == s2.length())
-            return (boolean)(s1==s2);
-        return false;
+        // cut the first char at the both strings
+        // if the s1[0] equals s2[0] cut them both
+        // if s1[0] != s2[0] cut only s1 and keep comparing
+        if(s1.charAt(0) == s2.charAt(0))
+            return daughter(s1.substring(1), s2.substring(1));
+        else
+            return daughter(s1.substring(1), s2);
     }
 
     public static void main(String[] args) {
-        String s1 = "ABC";
-        String s2 = "ABC";
+        String s1 = "Hello world";
+        String s2 = "Hllo wold";
         System.out.println(daughter(s1, s2));
     }
 }
