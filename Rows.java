67a8066c0648c289ad614c3471ba4c9575f39af5
idan-tankel <shakel002@gmail.com>
2021-01-24 00:44:03 +0200

    adding the rows aguments as a solution

--- Rows.java
+++ Rows.java
@@ -1,31 +1,46 @@
-class Rows{
+class Rows {
+
     public static int minAvgRow(int[][] a) {
-        return 0;
+        return minAvgRow(a, Double.MAX_VALUE, a.length - 1, a.length - 1);
     }
 
-    public static int minAvgRow(int[][] array, int tempavarage, int startIndex){
+    // solving the problem by passing to the method the avarage until now, and the
+    // suspected line until now by an argument
+    // the endIndex holds the last line that was not tested
+
+    public static int minAvgRow(int[][] array, Double minimumTempAvarage, int minimumTempLine, int endIndex) {
         // stop condition, the array has only one raw
-        if(array.length == 1)
-            return 1;
-        else{
-            // compute the avarageOnLastLine of the first line of the subarray
-            //the start index is the beginning of the subarray
-            double avarageOnLastLine = 0;
-            for(int i=0;i<array[array.length-1].length;i++){
-                avarageOnLastLine += array[array.length-1][i];
+        // end index was in the beginning array.length - 1
+        // the minimum line at the beginning was 0
+        // the minimumTempAvarage in the beginning was the highest level of int
+        if (endIndex < 0) // the array scan line by line was finished
+            return minimumTempLine;
+        else {
+            // compute the avarageOnTempLine of the first line of the subarray
+            // the start index is the beginning of the subarray
+            double avarageOnTempLine = 0;
+            for (int i = 0; i < array[endIndex].length; i++) {
+                avarageOnTempLine += array[endIndex][i];
             }
-            avarageOnLastLine = avarageOnLastLine/(array[array.length-1].length);
-            // get the most minimum avarageOnLastLine raw from the rest of the array
-            // to prevent array being manipulated, pass the last line of the test by an argument
-            // if the avarage on last line is lower than avarage on rest of the array -
-            return 0;
+            avarageOnTempLine = avarageOnTempLine / (array[endIndex].length);
+            // get the most minimum avarageOnTempLine raw from the rest of the array
+            // to prevent array being manipulated, pass the last line of the test by an
+            // argument
+            // if the avarage on temp line is lower than avarage on rest of the array
+            if (avarageOnTempLine <= minimumTempAvarage)
+                // run the method and update the minimum temp avarage and temp index
+                // endindex is now the line that holds the minimum
+                return minAvgRow(array, avarageOnTempLine, endIndex, endIndex - 1);
+            else
+                // the temp line avarage is higher than the avarage until now
+                return minAvgRow(array, minimumTempAvarage, minimumTempLine, endIndex - 1);
         }
     }
 
-    public static void main(String[] args){
-        int[][] arr = {{1,2,3}};
+    public static void main(String[] args) {
+        int[][] arr = { { 3, 2, 1, 4 }, { 7, 6, 5, -8 } };
         System.out.println(arr.length);
-        System.out.println(arr[arr.length-1].length);
-        System.out.println(minAvgRow(arr,1,1));
+        System.out.println(arr[arr.length - 1].length);
+        System.out.println(minAvgRow(arr));
     }
 }
--- Strings.java
+++ Strings.java
@@ -1,19 +1,19 @@
 public class Strings {
-    public static boolean daughter(String s1, String s2){
+    public static boolean daughter(String s1, String s2) {
         // if some of the strings are null for some reason
-        if(s2.length() == 0 || s2 == null)
+        if (s2.length() == 0 || s2 == null)
             return true;
 
-        if(s1 == null) // and s2 is not null because of the statement above
+        if (s1 == null) // and s2 is not null because of the statement above
             return false;
 
         // first, check the length of the two strings
-        if(s1.length()<s2.length())
+        if (s1.length() < s2.length())
             return false;
         // cut the first char at the both strings
         // if the s1[0] equals s2[0] cut them both
         // if s1[0] != s2[0] cut only s1 and keep comparing
-        if(s1.charAt(0) == s2.charAt(0))
+        if (s1.charAt(0) == s2.charAt(0))
             return daughter(s1.substring(1), s2.substring(1));
         else
             return daughter(s1.substring(1), s2);
